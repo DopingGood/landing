a568530aae132bc240b4a7c4be59ef9e14a3e4e7
DopingGood <nektomakseem@yandex.ru>
2015-07-11 14:37:21 +0400

    initial commit

---
+++ app/src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(".laptop-slicker").slick({
+		arrows: false,
+		swipe: false,
+		touchMove: true,
+		dots: true
+	});
+
+	$(".suggesting-prices").mouseover(function() {
+		$(this).addClass("price-hovered");
+	});
+	$(".suggesting-prices").mouseleave(function() {
+		$(this).removeClass("price-hovered");
+	});
+});
