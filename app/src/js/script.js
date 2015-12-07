1c06d9349cf6d569dbfbe773686e24ad45ff16d3
DopingGood <nektomakseem@yandex.ru>
2015-12-07 16:04:26 +0400

    scroll

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -16,54 +16,18 @@ $(document).ready(function(){
 		};
 	});
 
-	var $scroll = function (lg, md, sm, xs, ss) {
-		var windowWidth = $(window).width();
-		if (windowWidth > 1182) {
-			window.scrollTo(0, lg);
+	$(".navbut-service, .navbut-about-us, .navbut-pricing-table, .navbut-how-it, .navbut-happy-clients, .navbut-contact-us").click(function(e) {
+		e.preventDefault();
+		var dest = 0;
+		if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+			dest = $(document).height() - $(window).height();
 		}
-		else if (windowWidth > 974) {
-			window.scrollTo(0, md);
+		else {
+			dest = $(this.hash).offset().top;
 		}
-		else if (windowWidth > 750) {
-			window.scrollTo(0, sm);
-		}
-
-		else if (windowWidth > 588) {
-			window.scrollTo(0, xs);
-		}
-
-		else  {
-			window.scrollTo(0, ss);
-		};
-	};
-
-	$(".navbut-service").click(function(e){
-		e.preventDefault();
-		$scroll(800, 810, 830, 1050, 1090);
+		$('html,body').animate({
+			scrollTop: dest
+		}, 400, 'swing');
 	});
 
-	$(".navbut-about-us").click(function(e){
-		e.preventDefault();
-		$scroll(1350, 1400, 2500, 2700, 3050);
-	});
-
-	$(".navbut-pricing-table").click(function(e){
-		e.preventDefault();
-		$scroll(2050, 2120, 3200, 3280, 3800);
-	});
-
-	$(".navbut-how-it").click(function(e){
-		e.preventDefault();
-		$scroll(3050, 3140, 5300, 5400, 5900);
-	});
-
-	$(".navbut-happy-clients").click(function(e){
-		e.preventDefault();
-		$scroll(3900, 4000, 7100, 7200, 7700);
-	});
-
-	$(".navbut-contact-us").click(function(e){
-		e.preventDefault();
-		$scroll(4880, 5310, 0, 0, 0);
-	});
 });
